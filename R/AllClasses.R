bd132e52ac9f375e3b42de0b7df4d5c2170d4af7
Andres Sanz <sanz.andres@gmail.com>
2015-10-09 18:29:13 +0900

    Testing the devtools

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,2 +1,14 @@
 # AllClasses.R
 
+# Definition of the main class
+elm = setClass("elm",
+                   slots = c(inputs = "numeric",
+                             activation = "character",
+                             type = "character",
+                             bigdata = "logical"))
+
+# Testing the devtools
+elm1 = elm(inputs = 4,
+           activation = "sigmoid",
+           type = "I-ELM",
+           bigdata = FALSE)
