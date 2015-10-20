a698c75345d1b400927f212038d9f8eeeb07a9f6
rubenurraca <rubenurracavalle@gmail.com>
2015-10-20 13:36:31 +0200

    - Make use of the accesors instead of directly calling the slots - Rename variables and arguments accordying to the selected style - First draft of the prunning function (srtucture selection). The validation case was implemented
     Please enter the commit message for your changes. Lines starting

--- R/SLFNclass.R
+++ R/SLFNclass.R
@@ -12,22 +12,30 @@ setClass("SLFN",
                    outputs = "numeric",      # number of output features
                    neurons = "ANY",          # list of all neurons
                    beta = "ANY",             # weight vector outputs
-                   flist = "vector",         # neuron functions. (de momento character,)
-                   alpha = "numeric",        # normalization H'H solution
+                   act = "character",        # neuron functions. (de momento character,)
+                   alpha = "numeric",        # normalization H'H solution (ridge parameter)
+                   structureSelection = "logical", # tune number of neurons
+                   ranking = "character", # random/LASSO
+                   validation = "character", # none/V/CV/LOO
+                   folds = "numeric", # CV folds
                    batch = "integer",        # batch size of adaptive ELM
-                   classification= "character", # type of classification
-                   weights_wc = "ANY",      # weigths in weighted class.
+                   classification= "logical", # type of classification
+                   weights.wc = "ANY",      # weigths in weighted class.
                    time = "numeric",         # time of calculation
                    bigdata = "logical"),     # selection of acelerator
          prototype = prototype(inputs = integer(1),
                                outputs = integer(1),
                                neurons = NULL,
                                beta = NULL,
-                               flist = c("lin","sigm","tanh","rbf_l1","rbf_l2","rbf_linf"),
+                               act = c("lin"),
                                alpha = 1E-9,
+                               structureSelection = FALSE,
+                               ranking = "random",
+                               validation = "none",
+                               folds = 10,
                                batch = integer(10),
-                               classification= "None",
-                               weights_wc = NULL,
+                               classification= FALSE,
+                               weights.wc = NULL,
                                time = 0 ,
                                bigdata = FALSE))
 
@@ -36,8 +44,11 @@ setMethod("inputs","SLFN",function(object) return(object@inputs))
 setMethod("outputs","SLFN",function(object) return(object@outputs))
 setMethod("neurons","SLFN",function(object) return(object@neurons))
 setMethod("beta","SLFN",function(object) return(object@beta))
-setMethod("flist","SLFN",function(object) return(object@flist))
+setMethod("act","SLFN",function(object) return(object@act))
 setMethod("alpha","SLFN",function(object) return(object@alpha))
+setMethod("structureSelection","SLFN",function(object) return(object@structureSelection))
+setMethod("ranking","SLFN",function(object) return(object@ranking))
+setMethod("validation","SLFN",function(object) return(object@validation))
 setMethod("batch","SLFN",function(object) return(object@batch))
 setMethod("classification","SLFN",function(object) return(object@classification))
 setMethod("weights_wc","SLFN",function(object) return(object@weights_wc))
@@ -48,8 +59,11 @@ setReplaceMethod("inputs", "SLFN", function(x, value) { x@inputs <- value; x})
 setReplaceMethod("outputs", "SLFN", function(x, value) { x@outputs <- value; x})
 setReplaceMethod("neurons", "SLFN", function(x, value) { x@neurons <- value; x})
 setReplaceMethod("beta", "SLFN", function(x, value) { x@beta <- value; x})
-setReplaceMethod("flist", "SLFN", function(x, value) { x@flist <- value; x})
+setReplaceMethod("act", "SLFN", function(x, value) { x@act <- value; x})
 setReplaceMethod("alpha", "SLFN", function(x, value) { x@alpha <- value; x})
+setReplaceMethod("structureSelection","SLFN",function(x, value) { x@structureSelection <- value; x})
+setReplaceMethod("ranking","SLFN",function(x, value) { x@ranking <- value; x})
+setReplaceMethod("validation","SLFN",function(x, value) { x@validation <- value; x})
 setReplaceMethod("batch", "SLFN", function(x, value) { x@batch <- value; x})
 setReplaceMethod("classification", "SLFN", function(x, value) {x@classification <- value; x})
 setReplaceMethod("weights_wc", "SLFN", function(x, value) { x@weights_wc <- value; x})
@@ -69,12 +83,25 @@ setReplaceMethod("bigdata", "SLFN", function(x, value) { x@bigdata <- value; x})
 setMethod("show", "SLFN",
           function(object) {
             cat("A SLFN with: \n")
-            cat("      ",object@inputs, " inputs - ",
-                object@neurons, " ",
-                object@flist, "hidden neurons -",
-                object@outputs, "outputs", "\n")
-            cat("Training error: \n")
-            cat("Validation error: \n")
+            cat("      ",inputs(object), " inputs - ",
+                neurons(object), " ",
+                act(object), "hidden neurons -",
+                outputs(object), "outputs", "\n")
+            cat("Training scheme: \n")
+            if (structureSelection(object)){
+              cat("    Prunning = TRUE ")
+              if (ranking(object) == "random"){
+                cat ("Random ranking of neurons \n")
+              }else{
+                cat ("Ranking of neurons from LASSO \n")
+              }
+            }else{
+              cat("    Prunning = FALSE \n")
+            }
+            cat("    Validation =", validation(object), "\n")
+            cat("Errors: \n")
+            cat("    Training error: \n")
+            cat("    Validation error: \n")
           })
 
 
@@ -104,9 +131,11 @@ setMethod("train",
           def = function (object,
                           X = NULL,
                           Y = NULL,
-                          validation = "V",
-                          modelSelection = FALSE,
-                          classification = FALSE,
+                          structureSelection = FALSE,
+                          ranking = "random",
+                          validation = "none", #redundante. Tanto aquí como en el prototitpo
+                          classification = "none",
+                          folds = "10",
                           classType = "single",
                           ...) {
 
@@ -117,20 +146,29 @@ setMethod("train",
             # multi-label (ml) - n classes = n outpus. output above a thershold are selected
             # weighted (w) - uneven classes. they are weighted
 
-            # 2 - call project () - return H
+
+            # read training conditions
+            if (structureSelection) {
+              structureSelection(object) = structureSelection
+              ranking(object) = ranking
+              validation(object) = valdiation
+              if (validation == "CV")
+                  folds(object) = folds
+            }
 
             # 3 obtain beta. split model selection vs just training.
             # with model selection we have the option prunning P (aleatory rank of neurons), or OP (ranking based on LARS)
-            if (modelSelection == TRUE){# optimize number of neurons
-              if (validation == "V") {
-                # val. simple
-              } else if (validation == "CV"){
-                # CV
-              } else if (validation == "LOO"){
-                # LOO
+            if (structureSelection(object)){# optimize number of neurons
+              if (validation(object) == "V") {
+                trainV(object, X = X, Y = Y, X.v = X.v, Y.v = Y.v)
+              } else if (validation(object) == "CV"){
+                # CV algorithm
+              } else if (validation(object) == "LOO"){
+                # LOO algorithm
               }
-            } else if (modelSelection == FALSE) {
-              object@beta = algorithm (object, X = X, Y = Y, getBeta = TRUE)
+            } else { #here no validation make sense. just for computing errors...
+              H = project(object, X = X)
+              beta(object) = solveSystem(object, H = H, Y = Y, getBeta = TRUE)$beta
             }
             return(object)
             # 4 return errors ??? training error slot ?
@@ -139,72 +177,168 @@ setMethod("train",
 ##' @export
 setMethod("predict",
           signature = 'SLFN',
-          def = function (object,
-                          X = NULL) {
-            H = project(object, X=X)
-            Y = H %*% object@beta
-            return(Y)
+          def = function (object, X = NULL) {
+            H = project(object, X = X)
+            Yp = H %*% beta(object)
+            return(Yp)
           }
 )
 
 ##' @export
-algorithm <- function(object, X, Y, getBeta){
-  H = project(object, X = X)
-  beta = solveSystem(H = H, Y = Y, getBeta = TRUE)$beta
-  return(beta)
-}
+setMethod("project",
+          signature = "SLFN",
+          def = function(object, X = NULL){
+            ##' Compute the matrix H from X
+            ##' @param object
+            ##' @param X a matrix of dimensions [Nxd]; input matrix
+            ##' @return H a matrix of dimensions [NxL]; matrix after transformation
+            ##' @export
+            # random part (input weights)
+            if (act(object) == 'rbf') {
+              print("object@flist == 'rbf'")
+            } else {
+              # W a matrix of dimensions [dxL]; input weights
+              W = matrix( rnorm (inputs(object) * neurons(object), mean = 0, sd = 1), nrow = inputs(object), ncol = neurons(object))
+              # B a matrix of dimensions [Nx1]; input bias
+              B = rnorm (nrow(X), mean = 0, sd = 1)
+              # H0 a matrix of dimensions [NxL]; matrix before tranformation
+              H0 = X %*% W + B # could be implented in c++
+            }
+            # Transformation step:
+            if (act(object) == "linear"){
+              H = H0
+            } else if (act(object) == "sigmoid"){
+              H = 1 / (1 + exp(-H0))
+            } else if (act(object) == "tanH"){
+              H = tan(H0)
+            } else if (act(object) == "rbf"){
 
-##' Compute the matrix H from X
-##' @param object
-##' @param X a matrix of dimensions [Nxd]; input matrix
-##' @return H a matrix of dimensions [NxL]; matrix after transformation
-##' @export
-project <- function(object, X=NULL){
-  # random part (input weights)
-  if (object@flist == 'rbf') {
-    print("object@flist == 'rbf'")
-  } else {
-    # W a matrix of dimensions [dxL]; input weights
-    W = matrix( rnorm (object@inputs * object@neurons, mean = 0, sd = 1), nrow = object@inputs, ncol = object@neurons)
-    # B a matrix of dimensions [Nx1]; input bias
-    B = rnorm (nrow(X), mean = 0, sd = 1)
-    # H0 a matrix of dimensions [NxL]; matrix before tranformation
-    H0 = X %*% W + B # could be implented in c++
-  }
-  # Transformation step:
-  if (object@flist == "linear"){
-    H = H0
-  } else if (object@flist == "sigmoid"){
-    H = 1 / (1 + exp(-H0))
-  } else if (object@flist == "tanH"){
-    H = tan(H0)
-  } else if (object @flist == "rbf"){
+            }
+            return(H)
+          }
 
-  }
-  return(H)
-}
+)
 
-##' Solve the linear system H*beta = Y. Solve the system with otrhogonal
-##' projection - correlation matrices HH*beta=HT similar to .proj_cpu (akusok).
-##' May be it should be an S3 method...   params(object) <- newvalue
-##' @param H a matrix of dimensions [NxL] after transformation
-##' @param getBeta logical; needs to be true to return beta value
-##' @param Y a matrix of dimensions [Nxc] - output matrix (columns = nº variables or classes)
-##' @return beta a matrix of dimensions [Lxc] with the output weights
-##' @export
-solveSystem <- function(H, Y, getBeta = TRUE){
-  HH = t(H) %*% H  #     HH [LxL]
-  HT = t(H) %*% Y  #     HT [Lxc]
-  if (getBeta == TRUE) {
-    beta = solve (HH, HT) # base package
-    return(list("HH" = HH, "HT" = HT, "beta" = beta))
-  } else {
-    return(list("HH" = HH, "HT" = HT))
-  }
-}
+setMethod(f = "solveSystem",
+          signature = "SLFN",
+          def = function (object, H, Y, getBeta = TRUE){
+            ##' Solve the linear system H*beta = Y. Solve the system with otrhogonal
+            ##' projection - correlation matrices HH*beta=HT similar to .proj_cpu (akusok).
+            ##' May be it should be an S3 method...   params(object) <- newvalue
+            ##' @param H a matrix of dimensions [NxL] after transformation
+            ##' @param getBeta logical; needs to be true to return beta value
+            ##' @param Y a matrix of dimensions [Nxc] - output matrix (columns = nº variables or classes)
+            ##' @return beta a matrix of dimensions [Lxc] with the output weights
+            ##' @export
 
-#
+            HH = (t(H) %*% H) + diag(neurons(object)) * alpha(object) #     HH [LxL]
+            HT = t(H) %*% Y  #     HT [Lxc]
+            if (getBeta == TRUE) {
+              beta = solve (HH, HT) # base package
+              return(list("HH" = HH, "HT" = HT, "beta" = beta))
+            } else {
+              return(list("HH" = HH, "HT" = HT))
+            }
+          }
+)
 
 
+setMethod(f = "rankNeurons",
+          # require lars package
+          signature = "SLFN",
+          def = function (object, H = NULL, Y = NULL){
+            if (ranking(object) == "LASSO"){
+              rank = unlist(lars(x = H, y = Y, type = "lar")$actions)
+            } else {
+              rank = sample(1:neurons(object))
+            }
+            return(rank)
+          }
+)
 
+# MSE error
+setMethod(f = "error",
+          signature = "SLFN",
+          def = function(object, Y = NULL, Yp = NULL){
+            if (classification){
+            # classification case
+            } else {
+              if (validation(object) == "LOO"){
+                # LOO error
+              } else {
+                error = (sum ((Y - Yp)^2)) / length(y) # when dimension Y > 1 ????
+              }
+            }
+          return(error)
+          }
+)
+
+
+setMethod(f = "trainV",
+          signature = "SLFN",
+          def = function (object, X = NULL, Y = NULL, Xv = NULL, Yv =NULL) {
+            # structure selection = TRUE, validation = V
+
+            # train the model with neurons = nn_max
+            H = project(object, X = X) # train H with all neurons
+            beta = solveSystem(object, H = H, Y = Y, getBeta = TRUE)$beta
+            # parameters:
+            nnMax = neurons(object) # number of neurons (nn) max
+
+            # validation H
+            Hv = project(object, X = Xv)
+
+            nRank = rankNeurons(object, H = Hv, Y = Yv) # ranking of neurons (n.index = index of neurons)
+            Yv_pred = predict(object, X = Xv)
+            error = error(object, Y = Yv, Yp = Yv_pred) # validation MSE
+            penalty = ( error * 0.01 ) / nnMax # 1% of error at max nn
+
+            e = rep(-1, nnMax) # error vector for different number of neurons. initially filled with (-1)
+            e[nnMax] = error + nnMax * penalty
+
+            # minimization algorithm
+            A = 1 # min
+            E = nnMax # max
+            l = E- A # initial search interval
+
+            B = A + l/4
+            C = A + l/2
+            D = A + 3*l/4
+
+            while (l > 2) {
+              for (nn in c(A, B, C, D, E)) {
+                if (e[nn] == -1) {
+                  # auxiliar parameters for the case being computed
+                  object2 = object # create a copy of the elm to test the difference variants of nn
+                  H2 = H[, nRank[1:nn]] # creo que no pasa nada por hacerlo aquí en vez de en HH y HT
+                  beta(object2) = solveSystem (object2, H = H2 , Y = Yv)
+                  Yv_pred2 = predict(object2, X = Xv)
+                  e[nn] = error (object2, Y = Yv, Yp = Yv_pred2) + nn * penalty
+                }
+              }
+              # find minimum
+              m = min (e[A], e[B], e[C], e[D], e[E])
+              # halve the search interval
+              if (m %in% c(e[A], e[B])) {
+                E = C
+                C = B
+              } else if (m %in% c(e[D], e[E])) {
+                A = C
+                C = D
+              } else {
+                A = B
+                E = D
+              }
+              l = E - A
+              B = A + l/4
+              D = A + 3*l/4
+            }
+            nnOpt = which (c(e[A], e[B], e[C], e[D], e[E]) %in% m) # optimum number of neurons
+            # update model
+            object(neurons) = nnOpt
+            H = H[,nRank[1:nnOpt]]
+            beta(object) = solveSystem(object, H = H, Y = Y, getBeta = TRUE)$beta
+            return (object)
+          }
+)
 
